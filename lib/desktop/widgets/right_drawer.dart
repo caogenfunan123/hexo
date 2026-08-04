147d76641ad4a1c660935fc845684fd4314e87ca
caogenfunan123 <caogenfunan123@users.noreply.github.com>
2026-08-04 07:03:48 +0000

    fix: right_drawer/work_mode 需要同时 import + export 才能使用和导出

--- lib/desktop/widgets/right_drawer.dart
+++ lib/desktop/widgets/right_drawer.dart
@@ -3,6 +3,7 @@
 library;
 
 import 'package:flutter/material.dart';
+import '../../controllers/layout_controller.dart' show RightDrawerTab;
 export '../../controllers/layout_controller.dart' show RightDrawerTab;
 
 class DesktopRightDrawer extends StatelessWidget {
--- lib/desktop/widgets/work_mode.dart
+++ lib/desktop/widgets/work_mode.dart
@@ -1,4 +1,5 @@
 /// 工作模式枚举（从 LayoutController 导入）
 library;
 
+import '../../controllers/layout_controller.dart' show WorkMode;
 export '../../controllers/layout_controller.dart' show WorkMode;
